605701886c1cdfd2017872e4365af74179b7a64b
KARTIK SINGHAL <kartik124507@gmail.com>
2024-02-06 06:20:03 +0530

    admin

---
+++ adminportal/script.js
@@ -0,0 +1,33 @@
+function displayOption(option) {
+    var loginContent = document.getElementById("login-content");
+
+    
+    loginContent.innerHTML = "";
+
+    if (option === 'UserId') {
+        var userIdInput = document.createElement("input");
+        userIdInput.type = "text";
+        userIdInput.placeholder = "Enter User ID";
+        userIdInput.id = "user-id-input";
+
+       
+        var passwordInput = document.createElement("input");
+        passwordInput.type = "password";
+        passwordInput.placeholder = "Enter Password";
+        passwordInput.id = "password-input";
+
+        var loginButton = document.createElement("button");
+        loginButton.className = "login-button";
+        loginButton.textContent = "LOGIN";
+        loginButton.onclick = sendLogin;
+
+        loginContent.appendChild(userIdInput);
+        loginContent.appendChild(passwordInput);
+        loginContent.appendChild(loginButton);
+    }
+}
+
+function sendLogin() {
+   
+    alert("Login button clicked! Implement your login logic.");
+}
